28cfc9731b59d566272a2612d83db37a44703b4c
Jeff Minnear <jeffminnear@gmail.com>
2015-12-21 10:30:49 -0500

    Finish RegisteredApplication #index action and view

--- spec/features/user_views_registered_applications_spec.rb
+++ spec/features/user_views_registered_applications_spec.rb
@@ -14,4 +14,24 @@ RSpec.feature 'User views registered applications', js: true do
     expect(page).to have_title("Registered Applications")
     expect(page).to have_content("You have not registered any applications yet.")
   end
+
+  scenario "successfully with applications" do
+    user = create(:user)
+    applications = create_list(:application, 3, user: user)
+
+    visit new_user_session_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "Log in"
+
+    expect(page).to have_title("Registered Applications")
+    expect(page).not_to have_content("You have not registered any applications yet.")
+
+    applications.each do |application|
+      expect(page).to have_content(application.name)
+      expect(page).to have_content(application.url)
+    end
+  end
 end
