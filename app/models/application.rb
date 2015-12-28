1fdd0d6b6891085292868fb84a757409636eb3eb
Jeff Minnear <jeffminnear@gmail.com>
2015-12-28 10:41:12 -0500

    Add validations to Application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,9 @@
 class Application < ActiveRecord::Base
   belongs_to :user
   has_many :events
+
+  validates_presence_of :name, message: "can't be blank!"
+  validates_uniqueness_of :name, scope: :user_id, message: "is already in use (by you!)"
+  validates_presence_of :url, message: "can't be blank!"
+  validates_uniqueness_of :url, message: "already belongs to another application"
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Application, type: :model do
   it { should belong_to :user }
+  it { should validate_presence_of(:name).with_message(/can't be blank!/) }
+  it { should validate_presence_of(:url).with_message(/can't be blank!/) }
+  it { should validate_uniqueness_of(:url).with_message(/already belongs to another application/) }
 end
