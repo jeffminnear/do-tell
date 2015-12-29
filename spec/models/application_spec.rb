f23d4de8410df30f2310766d78e2923237b3365a
Jeff Minnear <jeffminnear@gmail.com>
2015-12-29 08:31:50 -0500

    Add test to validate uniqueness of application name scoped to user

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Application, type: :model do
   it { should belong_to :user }
   it { should validate_presence_of(:name).with_message(/can't be blank!/) }
+  it { should validate_uniqueness_of(:name).scoped_to(:user_id).with_message("is already in use (by you!)") }
   it { should validate_presence_of(:url).with_message(/can't be blank!/) }
   it { should validate_uniqueness_of(:url).with_message(/already belongs to another application/) }
 end
