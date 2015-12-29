5cebeeab911a670f0b6f79772029f1f25d8832d1
Jeff Minnear <jeffminnear@gmail.com>
2015-12-29 15:17:12 -0500

    Update API::EventsController

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,5 +1,14 @@
 class API::EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
+
+  before_action :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Content-Type'
+  end
 
   def create
     application = Application.find_by(url: request.env['HTTP_ORIGIN'])
@@ -15,6 +24,10 @@ class API::EventsController < ApplicationController
     else
       render json: "Unregistered application", status: :unprocessable_entity
     end
+  end
+
+  def preflight
+    head 200
   end
 
 
