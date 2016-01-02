693cda9c0c211dd59464ad722b9cf5d2cc844396
Jeff Minnear <jeffminnear@gmail.com>
2016-01-02 11:15:39 -0500

    Add testing of application delete confirmation dialog

--- spec/features/user_deletes_registered_application_spec.rb
+++ spec/features/user_deletes_registered_application_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature 'User deletes a registered application', js: true do
-  scenario 'Successfully' do
+RSpec.feature 'User attempts to delete a registered application', js: true do
+  scenario 'User Confirms' do
     user = create(:user)
     application = create(:application, user: user)
 
@@ -13,7 +13,26 @@ RSpec.feature 'User deletes a registered application', js: true do
 
     click_link "Delete Registered Application"
 
+    page.driver.browser.switch_to.alert.accept
+
     expect(page).to have_content("You have not registered any applications yet.")
     expect(page).to have_css('.flash', text: 'Application deleted successfully.')
+  end
+
+  scenario "User Cancels" do
+    user = create(:user)
+    application = create(:application, user: user)
+
+    login_user(user)
+
+    click_link application.name
+
+    expect(page).to have_title(application.name)
+
+    click_link "Delete Registered Application"
+
+    page.driver.browser.switch_to.alert.dismiss
+
+    expect(page).to have_content(application.name)
   end
 end
