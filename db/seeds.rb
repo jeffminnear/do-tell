49a40693fd5adf8125fcc0c00e06ca3cb7ee2a52
Jeff Minnear <jeffminnear@gmail.com>
2016-01-02 14:09:45 -0500

    Modify seeds.rb to always create a user with events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
+# Create a user with application and events
+user = User.create( email: Fake.email,
+                    password: "helloworld",
+                    confirmed_at: Time.zone.now
+)
+app_name = Fake.application_name
+application = Application.create( name: app_name,
+                                  url: Fake.url(app_name),
+                                  user: user
+)
+rand(4..8).times do
+  event = Event.create( name: Fake.event_name,
+                        application: application,
+                        created_at: Fake.time
+  )
+end
+
 # Create users
 20.times do
   user = User.create( email: Fake.email,
@@ -20,7 +37,8 @@ applications = Application.all
 # Create events
 200.times do
   event = Event.create( name: Fake.event_name,
-                        application: applications.sample
+                        application: applications.sample,
+                        created_at: Fake.time
   )
 end
 
